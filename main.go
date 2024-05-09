546550af090c9255ebf1bd148929bdd84994dd87
xu-yongjia <2359865877@qq.com>
2024-05-09 09:58:55 +0800

    原来的代码放回去到注释
    以为有合并检查的，没想到直接改了

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"backend/models"
 	"backend/mysql"
 	"backend/router"
-	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -41,7 +40,29 @@ func main() {
 func Execute() error {
 	// viper 用来取 config/dev.yaml中的参数
 	rootCmd.RunE = func(cmd *cobra.Command, args []string) error {
+		// _, err := mysql.DialWithPassword(
+		// 	viper.GetString("ssh.Host"),
+		// 	viper.GetInt("ssh.Port"),
+		// 	viper.GetString("ssh.User"),
+		// 	viper.GetString("ssh.Password"),
+		// )
 
+		// if err != nil {
+		// 	return err
+		// }
+
+		// _, err = mysql.Init(
+		// 	viper.GetString("db.hostname"),
+		// 	viper.GetInt("db.port"),
+		// 	viper.GetString("db.username"),
+		// 	viper.GetString("db.password"),
+		// 	viper.GetString("db.dbname"),
+		// ) // 用viper将对应的参数取出来
+		// if err != nil {
+		// 	return err
+		// }
+
+		// mysql.DB.AutoMigrate(&models.User{}, &models.PresetBackground{}) // 将数据库的表自动映射为User
 		var err error
 		mysql.DB, err = gorm.Open("mysql", "root:654321@tcp(47.121.29.57:3307)/mydb?charset=utf8mb4&parseTime=True&loc=Local")
 		if err != nil {
@@ -51,13 +72,6 @@ func Execute() error {
 			AutoMigrate(&models.User{}).
 			AutoMigrate(&models.PresetBackground{})
 
-		var users []models.User
-		if err := mysql.DB.Raw("SELECT * FROM user").Scan(&users).Error; err != nil {
-			panic("failed to scan users")
-		}
-
-		// 打印所有用户信息
-		fmt.Print(users)
 		// 最后别忘了把连接关了
 		defer mysql.DB.Close()
 		// defer mysql.SshDatabaseClient.Close()
