9a974a348c0eb44b9c3e1e5b5801ce02380d086d
bingbinggg <2286815465@qq.com>
2024-06-02 19:39:13 +0800

    api- new project table

--- control/data_sync.go
+++ control/data_sync.go
@@ -154,6 +154,14 @@ func NewProjectTable(c *gin.Context) {
 	var t table
 
 	if e := c.ShouldBindJSON(&t); e == nil {
+		// 判断是否已存在
+		var projectTable1 models.ProjectTable
+		if e := mysql.DB.Where("PU_uid = ? and PT_remote_db_name = ? and PT_remote_table_name = ?",
+			t.Uid, t.RemoteDbName, t.RemoteTableName).First(&projectTable1).Error; e == nil {
+			response.Fail(c, nil, "该项目表已存在!")
+			return
+		}
+		// 新增记录
 		var projectTable models.ProjectTable
 		projectTable = models.ProjectTable{
 			PT_name:              t.Name,
@@ -166,7 +174,6 @@ func NewProjectTable(c *gin.Context) {
 			PT_remote_password:   t.RemotePassword,
 			PT_remote_port:       t.RemotePort,
 		}
-
 		tx := mysql.DB.Begin()
 		if e := tx.Create(&projectTable).Error; e != nil {
 			tx.Rollback()
@@ -174,11 +181,107 @@ func NewProjectTable(c *gin.Context) {
 			return
 		}
 		if e := tx.Commit().Error; e != nil {
-			response.Fail(c, nil, "提交事务时出错")
+			response.Fail(c, nil, "插入新项目表时出错")
 			return
 		}
+		// 数据同步——获取表结构
+		tmp, err := gorm.Open("mysql",
+			fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+				t.RemoteUsername, t.RemotePassword, t.RemoteHostname, t.RemotePort, t.RemoteDbName))
+		if err != nil {
+			response.Fail(c, nil, "数据同步出错")
+			return
+		}
+		QueryCreateTableSQL := fmt.Sprintf("SHOW CREATE TABLE %s", t.RemoteTableName)
+		var createSQLResult []struct {
+			TableName   string `gorm:"column:Table"`
+			CreateTable string `gorm:"column:Create Table"`
+		}
+		if err := tmp.Raw(QueryCreateTableSQL).Scan(&createSQLResult).Error; err != nil {
+			response.Fail(c, nil, "数据同步出错")
+			return
+		}
+		leftIndex := strings.Index(createSQLResult[0].CreateTable, "(")
+		rightIndex := strings.LastIndex(createSQLResult[0].CreateTable, ")")
+		configStr := createSQLResult[0].CreateTable[leftIndex : rightIndex+1]
 
-		response.Success(c, gin.H{}, "")
+		re := regexp.MustCompile(`PRIMARY KEY \((.*?)\)`)
+		configStr1 := re.ReplaceAllStringFunc(configStr, func(s string) string {
+			return s + " NOT ENFORCED"
+		})
+		re = regexp.MustCompile(`\s*DEFAULT\s+[^,]*\s*,`)
+		configStr1 = re.ReplaceAllString(configStr1, ",")
+		configStr1 = strings.ReplaceAll(configStr1, "datetime", "timestamp")
+
+		var projectTable2 models.ProjectTable
+		if e := mysql.DB.Where("PU_uid = ? and PT_remote_db_name = ? and PT_remote_table_name = ?",
+			t.Uid, t.RemoteDbName, t.RemoteTableName).First(&projectTable2).Error; e == nil {
+			response.Fail(c, nil, "数据同步出错")
+			return
+		}
+		if err := tmp.Close(); err != nil {
+			response.Fail(c, nil, "数据同步出错")
+			return
+		}
+		// 数据同步——创建数据表结构
+		dropTableSQL := fmt.Sprintf("DROP TABLE IF EXISTS %d_%s_%s;", t.Uid, t.RemoteDbName, t.RemoteTableName)
+		if err := mysql.DB_flink.Exec(dropTableSQL).Error; err != nil {
+			response.Fail(c, nil, "数据同步出错")
+			return
+		}
+		createTableSQL := fmt.Sprintf("CREATE TABLE %d_%s_%s %s;", t.Uid, t.RemoteDbName, t.RemoteTableName, configStr)
+		if err := mysql.DB_flink.Exec(createTableSQL).Error; err != nil {
+			response.Fail(c, nil, "数据同步出错")
+			return
+		}
+		// 数据同步——java程序执行
+		strCreatDatabase := fmt.Sprintf("CREATE DATABASE IF NOT EXISTS flink_%s", t.RemoteDbName)
+		strSource := fmt.Sprintf("CREATE TABLE flink_%s.flink_source_%s "+
+			"%s"+ //主键必须标明NOT ENFORCED
+			" WITH (\n"+
+			"  'connector'  = 'mysql-cdc',\n"+
+			"  'hostname'   = '%s',\n"+
+			"  'port'   = '%d',\n"+
+			"  'database-name'   = '%s',\n"+
+			"  'table-name' = '%s',\n"+
+			"  'username'   = '%s',\n"+
+			"  'password'   = '%s'\n"+
+			")",
+			projectTable2.PT_remote_db_name, projectTable2.PT_remote_table_name, configStr1,
+			projectTable2.PT_remote_hostname, projectTable2.PT_remote_port, projectTable2.PT_remote_db_name,
+			projectTable2.PT_remote_table_name, projectTable2.PT_remote_username, projectTable2.PT_remote_password)
+		strTarget := fmt.Sprintf("CREATE TABLE flink_%s.flink_target_%s "+
+			"%s"+ //主键必须标明NOT ENFORCED
+			" WITH (\n"+
+			"  'connector'  = 'jdbc',\n"+
+			"  'driver'     = 'com.mysql.cj.jdbc.Driver',\n"+
+			"  'url'        = 'jdbc:mysql://47.121.29.57:3307/flink_target',\n"+
+			"  'table-name' = '%d_%s_%s',\n"+
+			"  'username'   = 'root',\n"+
+			"  'password'   = '654321'\n"+
+			")",
+			projectTable2.PT_remote_db_name, projectTable2.PT_remote_table_name, configStr1,
+			projectTable2.PU_uid, projectTable2.PT_remote_db_name, projectTable2.PT_remote_table_name)
+		strInsert := fmt.Sprintf("INSERT INTO flink_%s.flink_target_%s select * from flink_%s.flink_source_%s\n",
+			t.RemoteDbName, t.RemoteTableName, t.RemoteDbName, t.RemoteTableName)
+		// 运行java命令
+		cmd := exec.Command("java",
+			"-cp", "flinkdemo.jar;flink_libs/*",
+			"com.demo.flink.FlinkCdcMySql",
+			strCreatDatabase,
+			strSource,
+			strTarget,
+			strInsert,
+		)
+		if err = cmd.Start(); err != nil {
+			response.Fail(c, nil, "failed to start")
+			return
+		}
+		// 保存Process以便后续控制
+		process := cmd.Process
+		processMap.Store(projectTable2.PT_uid, process)
+
+		response.Success(c, gin.H{"id": projectTable2.PT_uid}, "")
 	} else { //JSON解析失败
 		response.Fail(c, nil, "数据格式错误!")
 	}
@@ -205,6 +308,7 @@ func DeleteProjectTable(c *gin.Context) {
 			if proc, ok := process.(*os.Process); ok {
 				// 终止进程
 				if err := proc.Kill(); err == nil {
+					processMap.Delete(m.Id)
 					response.Success(c, nil, "Success to kill process")
 					return
 				}
